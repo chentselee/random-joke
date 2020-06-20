2c8e0ca5203209df8c82cce8eff8a3434f4892e4
承澤李 <chentselee@gmail.com>
2020-06-20 22:05:47 +0800

    Title change

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
-      args[0].title = "Random joke";
+      args[0].title = "Random Joke";
       return args;
     });
   },
